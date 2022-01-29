7e25cb0773e0dd561395e5e196607085aa4b5db9
Averagegithubusername <s0mething_different@mail.ru>
2022-01-30 00:34:52 +0300

    code formatted according to remark

--- src/main/java/ru/netology/domain/TShirt.java
+++ src/main/java/ru/netology/domain/TShirt.java
@@ -3,21 +3,45 @@ package ru.netology.domain;
 import java.util.Objects;
 
 public class TShirt extends Product {
-  private String color;
-  private String size;
+    private String color;
+    private String size;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    if (!super.equals(o)) return false;
-    TShirt shirt = (TShirt) o;
-    return Objects.equals(color, shirt.color) &&
-        Objects.equals(size, shirt.size);
-  }
+    public String getColor() {
+        return color;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(super.hashCode(), color, size);
-  }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        TShirt shirt = (TShirt) o;
+        return Objects.equals(color, shirt.color) &&
+                Objects.equals(size, shirt.size);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), color, size);
+    }
+
+    @Override
+    public String toString() {
+        return "TShirt{" +
+                "color='" + color + '\'' +
+                ", size='" + size + '\'' +
+                '}';
+    }
 }
--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,38 +1,39 @@
 package ru.netology.repository;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.AssertionsKt;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.NotFoundException;
 import ru.netology.domain.Product;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class ProductRepositoryTest {
-  private ProductRepository repository = new ProductRepository();
-  private Book book1 = new Book(1, "Core Java", 500, "Smart Guy", 100, 2022);
-  private Book book2 = new Book(2, "Некрономикон", 1000, "Лавкрафт", 1000, 1961);
+    private ProductRepository repository = new ProductRepository();
+    private Book book1 = new Book(1, "Core Java", 500, "Smart Guy", 100, 2022);
+    private Book book2 = new Book(2, "Некрономикон", 1000, "Лавкрафт", 1000, 1961);
 
-  @BeforeEach
-  public void setUp() {
-    repository.save(book1);
-    repository.save(book2);
-  }
+    @BeforeEach
+    public void setUp() {
+        repository.save(book1);
+        repository.save(book2);
+    }
 
-  @Test
-  public void shouldRemoveById() {
-    repository.removeById(2);
+    @Test
+    public void shouldRemoveById() {
+        repository.removeById(2);
 
-    Product[] expected = new Product[]{book1};
-    Product[] actual = repository.findAll();
-    assertArrayEquals(expected, actual);
-  }
+        Product[] expected = new Product[]{book1};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
 
-  @Test
-  public void shouldThrowNotFoundException() {
+    @Test
+    public void shouldThrowNotFoundException() {
 
-    Assertions.assertThrows(NotFoundException.class, ()->{repository.removeById(3);});
-  }
+        Assertions.assertThrows(NotFoundException.class, () -> {
+            repository.removeById(3);
+        });
+    }
 }
